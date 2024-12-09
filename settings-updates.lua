8c36cec29fcce6d75bd198fe9778bffd21d7fc0f
lightningund <lightningundies0826@gmail.com>
2024-12-09 15:07:05 -0600

    moved the regroup setting to the top of the file and changed the orderings to match their layout in the code

--- settings-updates.lua
+++ settings-updates.lua
@@ -1,24 +1,34 @@
 data:extend({
 	{
+		type = "bool-setting",
+		name = "belt-reskin-regroup",
+		setting_type = "startup",
+		default_value = true,
+		order = "00",
+	}
+})
+
+data:extend({
+	{
 		type = "color-setting",
 		name = "belt-reskin-color",
 		setting_type = "startup",
 		default_value = {210, 180, 80},
-		order = "0a"
+		order = "1a"
 	},
 	{
 		type = "color-setting",
 		name = "belt-reskin-fast-color",
 		setting_type = "startup",
 		default_value = {210, 60, 60},
-		order = "0b"
+		order = "1b"
 	},
 	{
 		type = "color-setting",
 		name = "belt-reskin-express-color",
 		setting_type = "startup",
 		default_value = {80, 180, 209},
-		order = "0c"
+		order = "1c"
 	},
 })
 
@@ -29,20 +39,10 @@ if mods["space-age"] then
 			name = "belt-reskin-turbo-color",
 			setting_type = "startup",
 			default_value = {160, 180, 76},
-			order = "0d"
+			order = "1d"
 		}
 	})
 end
-
-data:extend({
-	{
-		type = "bool-setting",
-		name = "belt-reskin-regroup",
-		setting_type = "startup",
-		default_value = true,
-		order = "00",
-	}
-})
 
 if mods["5dim_transport"] then
 	for i = 4,10 do
@@ -63,35 +63,35 @@ if mods["UltimateBelts"] then
 			name = "belt-reskin-ultra-fast-color",
 			setting_type = "startup",
 			default_value = {0, 179, 12},
-			order = "1a"
+			order = "3a"
 		},
 		{
 			type = "color-setting",
 			name = "belt-reskin-extreme-fast-color",
 			setting_type = "startup",
 			default_value = {224, 0, 0},
-			order = "1b"
+			order = "3b"
 		},
 		{
 			type = "color-setting",
 			name = "belt-reskin-ultra-express-color",
 			setting_type = "startup",
 			default_value = {54, 4, 181},
-			order = "1c"
+			order = "3c"
 		},
 		{
 			type = "color-setting",
 			name = "belt-reskin-extreme-express-color",
 			setting_type = "startup",
 			default_value = {0, 43, 255},
-			order = "1d"
+			order = "3d"
 		},
 		{
 			type = "color-setting",
 			name = "belt-reskin-ultimate-color",
 			setting_type = "startup",
 			default_value = {0, 255, 221},
-			order = "1e"
+			order = "3e"
 		}
 	}
 
